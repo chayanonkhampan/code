a73ab3b2a7f6a4598c9489e9dfdef2452904eae8
minecraft2048bitgame <121738550+chayanonkhampan@users.noreply.github.com>
2023-12-06 15:03:37 +0700

    Update code.py

--- code.py
+++ code.py
@@ -3,15 +3,15 @@ import string
 import time
 
 def generate_random_code():
-  x_chars = ".join(random.choices(ascii_uppercase + string.digits, k=16))
-  code = f"RI-{x_chars[:4]}-{x_chars[4:8]}-{x_chars[8:12]}-{x_chars[12:16]}"
-  return code
-  def main():
-    while True:
-      random_code = generate_random_code()
-      print(random_code)
-      
-      time.sleep(1)
+    x_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
+    code = f"RI-{x_chars[:4]}-{x_chars[4:8]}-{x_chars[8:12]}-{x_chars[12:16]}"
+    return code
 
-if__name__=="__main__":
- main()
+def main():
+    while True:
+        random_code = generate_random_code()
+        print(random_code)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
